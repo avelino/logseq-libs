3d8346b02a0837830f72d29972a067fe36c1de2e
Avelino <avelinorun@gmail.com>
2024-01-03 18:53:23 -0300

    editor: register-slash-command
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject run.avelino/logseq-libs "0.1.3-0.0.14"
+(defproject run.avelino/logseq-libs "0.1.4-0.0.17"
   :url "https://github.com/avelino/logseq-libs"
   :description "@logseq/libs wrapper for cljs"
-  :license {:name "MIT"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:url "http://opensource.org/licenses/MIT"
+            :name "MIT"}
   :scm {:name "git" :url "https://github.com/avelino/logseq-libs"}
 
   :dependencies [[org.clojure/clojurescript "1.11.60"]]
 
-  :npm-deps {"@logseq/libs" "0.0.14"
-             :shadow-cljs "2.23.3"}
+  :npm-deps {"@logseq/libs" "0.0.17"
+             :shadow-cljs "2.26.2"}
   :install-deps true
 
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-codox "0.10.7"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [lein-codox "0.10.8"]]
 
   :codox {:language :clojurescript
           :source-paths ["src"]
--- src/logseq_libs/editor.cljs
+++ src/logseq_libs/editor.cljs
@@ -4,3 +4,4 @@
 (def get-current-block js/logseq.Editor.getCurrentBlock)
 (def get-editing-block-content js/logseq.Editor.getEditingBlockContent)
 (def update-block js/logseq.Editor.updateBlock)
+(def register-slash-command js/logseq.Editor.registerSlashCommand)
