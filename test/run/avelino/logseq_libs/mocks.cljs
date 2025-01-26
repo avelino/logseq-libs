ee2bfa7b62113501fa3712a4487c89a0ab0d5b51
Avelino <31996+avelino@users.noreply.github.com>
2025-01-25 22:36:07 -0300

    test: add editor test
    Add test coverage for editor functions:
    - Block operations (remove, insert, update, get)
    - Block editing (edit, select)
    - Events (on-input-selection-end)
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- test/run/avelino/logseq_libs/mocks.cljs
+++ test/run/avelino/logseq_libs/mocks.cljs
@@ -1,45 +1,6 @@
-(ns run.avelino.logseq-libs.mocks)
+(ns run.avelino.logseq-libs.mocks
+  (:require [run.avelino.logseq-libs.test-utils :as test-utils]))
 
-;; Mock LSPluginCore
-(def LSPluginCore
-  #js {:baseInfo (fn [] #js {:id "test-plugin"})})
-
-;; Mock logseq global object
-(def logseq
-  (let [app-obj (js-obj)
-        plugin-obj (js-obj)]
-
-    ;; Core methods
-    (set! (.-ready plugin-obj)
-          (fn [& args] nil))
-    (set! (.-provideModel plugin-obj)
-          (fn [& args] nil))
-    (set! (.-provideStyle plugin-obj)
-          (fn [& args] nil))
-    (set! (.-provideUI plugin-obj)
-          (fn [& args] nil))
-
-    ;; Settings methods
-    (set! (.-updateSettings plugin-obj)
-          (fn [& args] nil))
-    (set! (.-onSettingsChanged plugin-obj)
-          (fn [& args] nil))
-    (set! (.-beforeunload plugin-obj)
-          (fn [& args] nil))
-    (set! (.-useSettingsSchema plugin-obj)
-          (fn [& args] nil))
-
-    ;; App methods
-    (set! (.-registerCommandPalette app-obj)
-          (fn [& args] nil))
-    (set! (.-getUserConfigs app-obj)
-          (fn [] (js/Promise.resolve #js {:preferredLanguage "en"})))
-
-    ;; Set App object
-    (set! (.-App plugin-obj) app-obj)
-
-    plugin-obj))
-
-;; Export mock for use in tests
-(def LSPlugin #js {:LSPluginCore LSPluginCore
-                   :logseq logseq})
+;; Re-export test utils mocks
+(def LSPlugin test-utils/LSPlugin)
+(def logseq test-utils/logseq-libs-mock)
--- test/run/avelino/logseq_libs/test_runner.cljs
+++ test/run/avelino/logseq_libs/test_runner.cljs
@@ -1,6 +1,8 @@
 (ns run.avelino.logseq-libs.test-runner
   (:require [cljs.test :refer-macros [run-tests]]
-            [run.avelino.logseq-libs.core-test]))
+            [run.avelino.logseq-libs.core-test]
+            [run.avelino.logseq-libs.editor-test]))
 
 (defn ^:export run []
-  (run-tests 'run.avelino.logseq-libs.core-test))
+  (run-tests 'run.avelino.logseq-libs.core-test
+             'run.avelino.logseq-libs.editor-test))
